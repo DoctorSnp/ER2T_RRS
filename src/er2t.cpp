4a6cafc56e076a1780ae7d94592173bd89e1021e
DoctorSnp <den2011chs8@gmail.com>
2020-06-20 17:01:49 +0300

    Добавлены файлы

---
+++ src/er2t.cpp
@@ -0,0 +1,467 @@
+#include <QDir>
+
+#include "er2t.h"
+#include "train-horn.h"
+#include "filesystem.h"
+#include "sl2m.h"
+#include <QThread>
+
+
+static qint32 MAIN_RESERVOIR_VOLUME = 1200;
+
+static void printer (QString text)
+{
+    printf("%s\n", text.toStdString().c_str());
+    fflush(stdout);
+}
+
+
+Er2T::Er2T(QObject *parent) : Vehicle (parent)
+  , charge_press(0.0)
+{
+    printer("Make Er2t");
+    initialization();
+}
+
+Er2T::~Er2T()
+{
+    printer("Delete Er2t");
+}
+
+void Er2T::initBrakeDevices(double p0, double pTM, double pFL)
+{
+    main_reservoir->setY(0, pFL);
+    charge_press = p0;
+
+    load_brakes_config(config_dir + QDir::separator() + "brakes-init.xml");
+}
+
+
+void Er2T::initialization()
+{
+    FileSystem &fs = FileSystem::getInstance();
+    QString modules_dir = QString(fs.getModulesDir().c_str());
+    printer("Initializing Er2T");
+    initSupplyMachines();
+
+
+
+    m_initBrakeControls(modules_dir);
+    m_initBrakeMechanics();
+
+    m_initBrakeEquipment(modules_dir);
+
+    initOtherEquipment();
+
+    printer("Er2T is initialized!");
+}
+
+void Er2T::initOtherEquipment()
+{
+    //static int count = 1;
+    //DebugMsg = "tadm : " + QString::number(count);
+    //printer("11111");
+    //if (m_speedMeter == nullptr)
+   //     m_speedMeter = new SL2M();
+
+    //printer("22222");
+
+   //m_speedMeter->setWheelDiameter(wheel_diameter);
+   //m_speedMeter->read_custom_config(config_dir + QDir::separator() + "3SL-2M");
+    //connect(m_speedMeter, &SL2M::soundSetVolume, this, &SimpleLoco::soundSetVolume);
+    if (horn == nullptr) {
+        horn = new TrainHorn(this);
+        connect(horn, &TrainHorn::soundSetVolume, this, &Er2T::soundSetVolume);
+    }
+}
+
+void Er2T::initSupplyMachines()
+{
+    m_phase_spliter = new PhaseSplitter();
+    connect(m_phase_spliter, &PhaseSplitter::soundSetPitch, this, &Er2T::soundSetPitch);
+
+    for (size_t i = 0; i < m_motor_fans.size(); ++i)
+    {
+        m_motor_fans[i] = new MotorFan(i + 1);
+        connect(m_motor_fans[i], &MotorFan::soundSetPitch, this, &Er2T::soundSetPitch);
+    }
+
+    main_reservoir = new Reservoir(static_cast<double>(MAIN_RESERVOIR_VOLUME) / 1000.0);
+
+    QString mk_cfg_path = config_dir + QDir::separator() + "motor-compressor.xml";
+    motor_compressor = new MotorCompressor(mk_cfg_path);
+    connect(motor_compressor, &MotorCompressor::soundSetPitch, this, &Er2T::soundSetPitch);
+
+    m_press_reg = new PressureRegulator();
+}
+
+void Er2T::m_initBrakeEquipment(QString modules_dir)
+{
+    switch_valve = new SwitchingValve();
+    switch_valve->read_config("zpk");
+
+    pneumo_relay = new PneumoReley();
+    pneumo_relay->read_config("rd304");
+
+    pneumo_splitter = new PneumoSplitter();
+    pneumo_splitter->read_config("pneumo-splitter");
+    supply_reservoir = new Reservoir(0.078);
+
+
+    FileSystem &fs = FileSystem::getInstance();
+    brake_mech = loadBrakeMech(modules_dir + fs.separator() + "carbrakes-mech");
+    air_dist = loadAirDistributor(modules_dir + fs.separator() + "vr242");
+
+    air_dist->read_config("vr242");
+
+    //brake_mech->read_config("carbrakes-mech-composite");
+    brake_mech->read_custom_config(config_dir + QDir::separator() + "motor-brakes-mech");
+}
+
+void Er2T::load_brakes_config(QString path)
+{
+    CfgReader cfg;
+
+    if (cfg.load(path))
+    {
+        QString secName = "BrakesState";
+
+        double pFL = 0.0;
+
+        if (cfg.getDouble(secName, "MainReservoirPressure", pFL))
+        {
+            main_reservoir->setY(0, pFL);
+        }
+
+        double k_flow = 0.0;
+
+        if (cfg.getDouble(secName, "MainReservoirFlow", k_flow))
+        {
+            main_reservoir->setFlowCoeff(k_flow);
+        }
+
+        double ch_press = 0.0;
+
+        if (cfg.getDouble(secName, "ChargingPressure", ch_press))
+        {
+            charge_press = ch_press;
+        }
+
+        int train_crane_pos = 6;
+
+        if (cfg.getInt(secName, "TrainCranePos", train_crane_pos))
+        {
+            brake_crane->setPosition(train_crane_pos);
+        }
+
+        int loco_crane_pos = 0;
+
+        if (cfg.getInt(secName, "LocoCranePos", loco_crane_pos))
+        {
+            loco_crane->setHandlePosition(loco_crane_pos);
+        }
+
+        int brake_lock = 0;
+
+        int combine_crane_pos = -1;
+
+        if (cfg.getInt(secName, "CombineCranePos", combine_crane_pos))
+        {
+            ubt->setCombineCranePos(combine_crane_pos);
+        }
+
+        if (cfg.getInt(secName, "BrakeLockDevice", brake_lock))
+        {
+            ubt->setState(brake_lock);
+
+            if (brake_lock == 1)
+            {
+                ubt->setY(0, charge_press);
+                brake_crane->init(charge_press, pFL);
+                supply_reservoir->setY(0, charge_press);
+            }
+        }
+    }
+}
+
+double Er2T::trac_char(double v)
+{
+    // Переводим номинальную скорость в м/с
+         double v_nom = V_nom / Physics::kmh;
+         double traction_force = 0;
+
+         // Вычисляем силу тяги, в зависимости от величины
+         // текущей скорости по отношению к номинальной.
+         // Учитываем, что параметры заданной характеристики мы брали в кН,
+         // а движку требуются величины в системе СИ, поэтому домножаем на 1000
+         // переводя килоньютоны в ньютоны
+         if (abs(v) < v_nom)
+         {
+             traction_force = (F_max + (V_nom - F_max) * abs(v) / v_nom) * 1000.0;
+         }
+         else
+         {
+             traction_force = V_nom * v_nom * 1000.0 / v;
+         }
+
+         return traction_force;
+}
+
+void Er2T::keyProcess()
+{
+    if (getKeyState(KEY_A))
+    {
+        ref_traction_level += 0.01;
+    }
+
+    if (getKeyState(KEY_D))
+    {
+        ref_traction_level -= 0.01;
+    }
+
+    if (getKeyState(KEY_B))
+    {
+        printer("Svistok!");
+        emit soundSetVolume("Svistok", 100);
+    }
+
+    // Включение/выключение мотор-компрессора
+    if (getKeyState(KEY_E))
+    {
+        if (isShift())
+            m_mk_tumbler.set();
+        else
+            m_mk_tumbler.reset();
+    }
+
+    ref_traction_level = cut(ref_traction_level, 0.0, 1.0);
+    //DebugMsg = QString("Сила тяги : %1")
+    //        .arg(ref_traction_level, 4, 'f', 2);
+}
+
+void Er2T::loadConfig(QString cfg_path)
+{
+    // Создаем экземпляр "читателя" XML-конфигов
+         CfgReader cfg;
+
+         // Открываем конфигурационный файл по переданному движком пути
+         if (cfg.load(cfg_path))
+         {
+             // Задаем имя секции конфига, из которой будем читать параметры
+             QString sectionName = "Vehicle";
+
+             // Читаем интересующие нас параметы в соотвествующие переменные
+             cfg.getDouble(sectionName, "F_max", F_max);
+             cfg.getDouble(sectionName, "F_min", F_min);
+             cfg.getDouble(sectionName, "V_nom", V_nom);
+         }
+}
+
+void Er2T::m_initBrakeControls(QString modules_dir)
+{
+    ubt = new BrakeLock();
+    ubt->read_config("ubt367m");
+   // connect(ubt, &BrakeLock::soundPlay, this, &VL60::soundPlay);
+
+    brake_crane = loadBrakeCrane(modules_dir + QDir::separator() + "krm395");
+    brake_crane->read_config("krm395");
+   // connect(brake_crane, &BrakeCrane::soundPlay, this, &VL60::soundPlay);
+
+    loco_crane = loadLocoCrane(modules_dir + QDir::separator() + "kvt254");
+    loco_crane->read_config("kvt254");
+}
+
+void Er2T::m_initBrakeMechanics()
+{
+    m_trolley_mech[TROLLEY_FWD] = new TrolleyBrakeMech(config_dir +
+                                           QDir::separator() +
+                                           "fwd-trolley-brake-mech.xml");
+
+    m_trolley_mech[TROLLEY_BWD] = new TrolleyBrakeMech(config_dir +
+                                           QDir::separator() +
+                                                       "bwd-trolley-brake-mech.xml");
+}
+
+void Er2T::m_initTriggers()
+{
+    m_triggers.push_back(&fr_tumbler);
+    m_triggers.push_back(&m_mk_tumbler);
+
+    for (size_t i = 0; i < m_mv_tumblers.size(); ++i)
+        m_triggers.push_back(&m_mv_tumblers[i]);
+
+}
+
+void Er2T::m_stepPhaseSplitter(double t, double dt)
+{
+    //double U_power = trac_trans->getU_sn() *
+    //        static_cast<double>(fr_tumbler.getState());
+    //m_phase_spliter->setU_power(U_power);
+    m_phase_spliter->step(t, dt);
+}
+
+void Er2T::m_stepMotorCompressor(double t, double dt)
+{
+    main_reservoir->setAirFlow(motor_compressor->getAirFlow());
+    //main_reservoir->setAirFlow(50000.0);
+    main_reservoir->step(t, dt);
+
+    motor_compressor->setExternalPressure(main_reservoir->getPressure());
+    motor_compressor->setU_power(m_phase_spliter->getU_out() * static_cast<double>(m_mk_tumbler.getState()) * m_press_reg->getState());
+    motor_compressor->step(t, dt);
+
+    m_press_reg->setPressure(main_reservoir->getPressure());
+    m_press_reg->step(t, dt);
+}
+
+void Er2T::m_stepBrakeControl(double t, double dt)
+{
+
+    // Подключаем к УБТ трубопровод от ГР
+    ubt->setLocoFLpressure(main_reservoir->getPressure());
+    // Подключаем к УБТ трубопровод ТМ от КрМ
+    ubt->setCraneTMpressure(brake_crane->getBrakePipeInitPressure());
+    ubt->setControl(keys);
+    // Задаем давление в начале ТМ
+    p0 = ubt->getLocoTMpressure();
+    ubt->step(t, dt);
+
+    brake_crane->setFeedLinePressure(ubt->getCraneFLpressure());
+    brake_crane->setChargePressure(charge_press);
+    brake_crane->setBrakePipePressure(pTM);
+    brake_crane->setControl(keys);
+    brake_crane->step(t, dt);
+
+    loco_crane->setFeedlinePressure(ubt->getCraneFLpressure());
+    loco_crane->setBrakeCylinderPressure(switch_valve->getPressure2());
+    loco_crane->setAirDistributorFlow(0.0);
+    loco_crane->setControl(keys);
+    loco_crane->step(t, dt);
+}
+
+void Er2T::m_stepMotorFans(double t, double dt)
+{
+    for (size_t i = 0; i < NUM_MOTOR_FANS; ++i)
+    {
+        MotorFan *mf = m_motor_fans[i];
+        mf->setU_power(m_phase_spliter->getU_out() *
+                       static_cast<double>(m_mv_tumblers[i].getState()));
+        mf->step(t, dt);
+    }
+}
+
+void Er2T::stepTrolleysBrakeMech(double t, double dt)
+{
+    switch_valve->setInputFlow1(air_dist->getBrakeCylinderAirFlow());
+    switch_valve->setInputFlow2(loco_crane->getBrakeCylinderFlow());
+    switch_valve->setOutputPressure(pneumo_splitter->getP_in());
+    switch_valve->step(t, dt);
+
+    // Тройник подключен к ЗПК
+    pneumo_splitter->setQ_in(switch_valve->getOutputFlow());
+    pneumo_splitter->setP_out1(pneumo_relay->getWorkPressure());
+    pneumo_splitter->setP_out2(m_trolley_mech[TROLLEY_BWD]->getBrakeCylinderPressure());
+    pneumo_splitter->step(t, dt);
+
+    pneumo_relay->setPipelinePressure(main_reservoir->getPressure());
+    pneumo_relay->setWorkAirFlow(pneumo_splitter->getQ_out1());
+    pneumo_relay->setBrakeCylPressure(m_trolley_mech[TROLLEY_FWD]->getBrakeCylinderPressure());
+    pneumo_relay->step(t, dt);
+
+    // Передняя тележка наполняется через реле давления 304
+    printer( "SOME: " + QString::number(pneumo_relay->getBrakeCylAirFlow()));
+
+    //m_trolley_mech[TROLLEY_FWD]->setAirFlow(pneumo_relay->getBrakeCylAirFlow());
+    m_trolley_mech[TROLLEY_FWD]->setAirFlow(0.00530924);
+    m_trolley_mech[TROLLEY_FWD]->setVelocity(velocity);
+    m_trolley_mech[TROLLEY_FWD]->step(t, dt);
+
+    // Задняя тележка подключена через тройник от ЗПК
+   // m_trolley_mech[TROLLEY_BWD]->setAirFlow(pneumo_splitter->getQ_out2());
+     m_trolley_mech[TROLLEY_BWD]->setAirFlow(0.00530924);
+    m_trolley_mech[TROLLEY_FWD]->setVelocity(velocity);
+    m_trolley_mech[TROLLEY_BWD]->step(t, dt);
+
+    Q_r[1] = m_trolley_mech[TROLLEY_FWD]->getBrakeTorque();
+    Q_r[2] = m_trolley_mech[TROLLEY_FWD]->getBrakeTorque();
+    Q_r[3] = m_trolley_mech[TROLLEY_FWD]->getBrakeTorque();
+    Q_r[4] = m_trolley_mech[TROLLEY_BWD]->getBrakeTorque();
+    Q_r[7] = m_trolley_mech[TROLLEY_BWD]->getBrakeTorque();
+    Q_r[6] = m_trolley_mech[TROLLEY_BWD]->getBrakeTorque();
+    QString str = "Brake1: "+ QString::number(Q_r[1]) + " Brake2: "+ QString::number(Q_r[2]);
+    printer(str);
+}
+
+void Er2T::stepAirDistributors(double t, double dt)
+{
+    supply_reservoir->setAirFlow(air_dist->getAirSupplyFlow());
+    //supply_reservoir->setAirFlow(50.0);
+    supply_reservoir->step(t, dt);
+
+    air_dist->setBrakeCylinderPressure(switch_valve->getPressure1());
+    air_dist->setAirSupplyPressure(supply_reservoir->getPressure());
+    air_dist->setBrakepipePressure(pTM);
+    //air_disr->setBrakeCylinderPressure(50.0);
+    //air_disr->setAirSupplyPressure(50.0);
+    //air_disr->setBrakepipePressure(50.0);
+    auxRate = air_dist->getAuxRate();
+    air_dist->step(t, dt);
+}
+
+void Er2T::m_stepOtherEquipment(double t, double dt)
+{
+    horn->setControl(keys);
+    horn->step(t, dt);
+}
+
+void Er2T::step(double t, double dt)
+{
+    printer("STEP!");
+    // Вычисляем силу тяги, которую реализует локомотив в данный момент
+    double trac_force = ref_traction_level * trac_char(velocity);
+
+    // Вычисляем момент, приходящийся на одну колесную пару
+    double torque = trac_force * wheel_diameter / 2.0 / num_axis;
+    for (size_t i = 1; i < Q_a.size(); ++i)
+    {
+        Q_a[i] = ref_traction_level * torque;
+    }
+
+    m_stepPhaseSplitter(t, dt);
+
+    m_stepMotorFans(t, dt);
+
+    m_stepMotorCompressor(t, dt);
+
+    m_stepBrakeControl(t, dt);
+
+    stepTrolleysBrakeMech(t, dt);
+
+    stepAirDistributors(t, dt);
+
+    m_stepOtherEquipment(t, dt);
+
+   /* if ((velocity * Physics::kmh) >= 10.0)
+    {
+        printer("Braking!!!!\n");
+        pTM = 1000.0;
+        m_trolley_mech[TROLLEY_FWD]->setAirFlow(100.0);
+        setBrakepipePressure(100.0);
+        m_trolley_mech[TROLLEY_FWD]->setVelocity(velocity);
+    }*/
+
+    DebugMsg = QString("Гл.рез:%1 Зад. тяга:%2 Скор:%3 С.тяги:%4  Давл:%5 ТЦ1:%6 ТЦ2:%7 Колодки:%8 pTM:%9")
+           .arg(main_reservoir->getPressure(), 4, 'f', 2)
+            .arg(ref_traction_level, 4, 'f', 2)
+            .arg(velocity * Physics::kmh, 6, 'f', 2)
+            .arg(trac_force / 1000.0, 6, 'f', 1)
+            .arg(supply_reservoir->getPressure(), 4, 'f', 2)
+            .arg(m_trolley_mech[TROLLEY_FWD]->getBrakeCylinderPressure(), 4, 'f', 2)
+            .arg(m_trolley_mech[TROLLEY_BWD]->getBrakeCylinderPressure(), 4, 'f', 2)
+            .arg(m_trolley_mech[TROLLEY_FWD]->getShoeForce() / 1000.0, 5, 'f', 1)
+            .arg(pTM, 5, 'f', 1);
+            ;
+    DebugMsg += "|| " + QString::number(F_max);
+}
+
+GET_VEHICLE(Er2T)
---
+++ src/er2t.h
@@ -0,0 +1,159 @@
+#ifndef TEST_LOCO_H
+#define TEST_LOCO_H
+
+#include "vehicle-api.h"
+#include "ubt367m.h"
+#include "reservoir.h"
+/* пневматика */
+#include "pneumo-splitter.h"
+#include "pneumatic/trolley-brake-mech.h"
+#include "pneumatic/motor-fan.h"
+#include "pneumatic/motor-compressor.h"
+#include "pneumatic/pressure-regulator.h"
+
+#include "network.h"
+/*электрика*/
+
+#include "electric/phase-splitter.h"
+
+#include    "brake-mech.h" // Модель тормозной рычажной передачи
+#include    "reservoir.h" // Модель резервуара
+#include    "airdistributor.h" // Базовый класс "воздухораспределитель"
+
+
+class SL2M;
+
+class Er2T : public Vehicle
+{
+ public:
+     Er2T(QObject *parent = Q_NULLPTR);
+     ~Er2T() override;
+     /// Инициализация тормозных приборов
+     void initBrakeDevices(double p0, double pTM, double pFL) override;
+ private:
+
+     /* инициализация*/
+    void initialization() override;
+    void initSupplyMachines();
+    void m_initBrakeEquipment(QString modules_dir);
+    void load_brakes_config(QString path);
+    void initOtherEquipment();
+     // ======== устройства
+     /// Локомотивный скоростемер
+    /// Переключательный клапан ЗПК
+    SwitchingValve  *switch_valve;
+
+    /// Реле давления усл. №304
+    PneumoReley     *pneumo_relay;
+
+    /// Разветвитель трубопроводов (тройник)
+    PneumoSplitter  *pneumo_splitter;
+   // SL2M    *m_speedMeter = nullptr;
+    TrainHorn   *horn = nullptr;
+
+    // Тормозная рычажная передача
+    BrakeMech   *brake_mech;
+
+    // Запасный резервуар
+    Reservoir   *supply_reservoir;
+
+    // Воздухан
+    AirDistributor *air_dist;
+
+    enum
+    {
+        NUM_MOTOR_FANS = 2,
+        MV1 = 0,
+        MV2 = 1,
+    };
+
+    /// Регулятор давления в ГР
+    PressureRegulator *m_press_reg;
+
+    /// Асинхронный расщепитель фаз
+    PhaseSplitter   *m_phase_spliter;
+
+    /// Мотор-вентиляторы
+    std::array<MotorFan *, NUM_MOTOR_FANS> m_motor_fans;
+
+    /// Мотор-компрессор
+    MotorCompressor *motor_compressor;
+
+    /// Главный резервуар
+    Reservoir   *main_reservoir;
+    /// Зарядное давление
+    double  charge_press;
+    /// Устройство блокировки тормозов усл. №367М
+    BrakeLock   *ubt;
+    /// Поездной кран машиниста (КрМ)
+    BrakeCrane  *brake_crane;
+    /// Кран впомогательного тормоза (КВТ)
+    LocoCrane   *loco_crane;
+
+    double trac_char(double v);
+    /// Шаг моделирования систем локомотива
+    void step(double t, double dt) override;
+    // Обработка нажатия клавиш
+    void keyProcess() override;
+    // Загрузка пользовательских параметров из конфига
+    void loadConfig(QString cfg_path) override;
+
+    /// Инициализация приборов управления тормозами
+    void m_initBrakeControls(QString modules_dir);
+
+    /// Инициализация тормозной рычажной передачи
+    void m_initBrakeMechanics();
+
+    void m_initTriggers();
+
+
+    void m_stepPhaseSplitter(double t, double dt);
+
+    void m_stepMotorCompressor(double t, double dt);
+
+    void m_stepBrakeControl(double t, double dt);
+
+    void m_stepMotorFans(double t, double dt);
+
+
+    void stepTrolleysBrakeMech(double t, double dt);
+
+    void stepAirDistributors(double t, double dt);
+
+    // пошаговое выполнение
+    void m_stepOtherEquipment(double t, double dt);
+    // Максимальная реализуемая сила тяги, кН
+    double F_max = 450.0;
+     /// Номинальная сила тяги (соотвествующая продолжительной мощности), кН
+    double F_min = 350.0;
+     /// Номинальная скорость (соотвествующая часовой мощности), км/ч
+    double V_nom = 80.0;
+    /// Заданный уровень тягового усилия
+    double ref_traction_level = 0.0;
+
+    enum
+    {
+        NUM_TROLLEYS = 2,
+        TROLLEY_FWD = 0,
+        TROLLEY_BWD = 1
+    };
+
+    /// Тяговый трансформатор
+    //TracTransformer *trac_trans;
+
+    std::vector<Trigger *> m_triggers;
+
+    /// Тормозные механизмы тележек
+    std::array<TrolleyBrakeMech *, NUM_TROLLEYS> m_trolley_mech;
+
+    /// Триггеры тумблеров управления мотор-вентиляторами
+    std::array<Trigger, NUM_MOTOR_FANS> m_mv_tumblers;
+
+
+    /// Тригер тумблера "Фазорасщепитель"
+    Trigger fr_tumbler;
+    /// Тригер тумблера управления мотор-компрессором
+    Trigger m_mk_tumbler;
+ };
+
+#endif // TEST_LOCO_CPP
